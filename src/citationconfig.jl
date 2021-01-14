b6ca2147c6b09cb757c974e239c142ceb17b73e9
Neel Smith <dnsmith.neel@gmail.com>
2021-01-14 10:47:07 -0500

    Add all functions to DataFrame definition

--- src/citationconfig.jl
+++ src/citationconfig.jl
@@ -6,6 +6,12 @@ function citation_df(repo::EditingRepository)
 	arr = CSV.File(repo.root * "/" * repo.configs * "/citation.cex", skipto=2, delim="|") |> Array
 	urns = map(row -> CtsUrn(row[1]), arr)
 	files = map(row -> row[2], arr)
-	fnctns = map(row -> eval(Meta.parse(row[3])), arr)
-	DataFrame(urn = urns, file = files, converter = fnctns)
+	ohco2s = map(row -> eval(Meta.parse(row[3])), arr)
+	dipls = map(row -> eval(Meta.parse(row[4])), arr)
+	norms = map(row -> eval(Meta.parse(row[5])), arr)
+	orthos = map(row -> eval(Meta.parse(row[6])), arr)
+
+	DataFrame(urn = urns, file = files, 
+	o2converter = ohco2s, diplomatic = dipls,
+	normalized = norms, orthography = orthos)
 end
