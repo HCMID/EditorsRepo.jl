e684988735964c3e06e965630119ffc7d3a0a2e7
Neel Smith <dnsmith.neel@gmail.com>
2022-01-30 05:49:40 -0500

    Set up testing for split document repo

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,10 +8,14 @@ using TypedTables
 using PolytonicGreek, Lycian
 using Test
 
+#=
 include("test_repository.jl")
 include("test_citationschemes.jl")
 include("test_textcatalog.jl")
 include("test_xmlfiles.jl")
+=#
+include("test_splitdocs.jl")
+
 
 #=
 include("test_archivalcorpus.jl")
@@ -20,7 +24,7 @@ include("test_dse.jl")
 
 include("test_configvalidation.jl")
 include("test_texthandling.jl")
-include("test_splitdocs.jl")
+
 include("test_tokenization.jl")
 include("test_badeditions.jl")
 =#
