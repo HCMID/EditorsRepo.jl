40158e661dc8bdf72ef2bda938c3c89678f89655
Documenter.jl <documenter@juliadocs.github.io>
2024-06-17 17:12:12 +0000

    build based on a2056bc

---
+++ v0.19.3/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#EditorsRepo.jl","page":"Home","title":"EditorsRepo.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Manage editing of digital scholarly editions.","category":"page"},{"location":"","page":"Home","title":"Home","text":"Documentation has moved.","category":"page"}]
+}
--- versions.js
+++ versions.js
@@ -1,5 +1,6 @@
 var DOC_VERSIONS = [
   "stable",
+  "v0.19",
   "v0.18",
   "v0.17",
   "v0.16",
@@ -14,5 +15,5 @@ var DOC_VERSIONS = [
   "v0.7",
   "dev",
 ];
-var DOCUMENTER_NEWEST = "v0.18.9";
+var DOCUMENTER_NEWEST = "v0.19.3";
 var DOCUMENTER_STABLE = "stable";
